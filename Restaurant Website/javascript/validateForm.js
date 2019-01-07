35bed35db0d455ad891aa3345fd31a93687098c5
Ashley Shelden <aeshelden@gmail.com>
2019-01-07 18:38:36 -0500

    jQuery update 1/7

--- Restaurant Website/javascript/validateForm.js
+++ Restaurant Website/javascript/validateForm.js
@@ -1,12 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const sendRequest = document.getElementById('sendRequest');
-  sendRequest.addEventListener('submit', (e) => {
-    e.preventDefault;
-    const name = document.getElementsByClassName('name').value;
-    const email = document.getElementsByClassName('email').value;
-    const phone = document.getElementsByClassName('phone').value;
-    if( name !=='' || email !=='' || phone !==''){
-      alert("Your message has been received! You'll hear from us shortly.");
-    }
-  })
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   var sendRequest = document.getElementById('sendRequest');
+//   sendRequest.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var phone = document.getElementById('phone').value;
+//     if( name !=='' || email !=='' || phone !==''){
+//       alert("Your message has been received! You'll hear from us shortly.");
+//     }
+//   })
+// })
+
+$(document).ready(function() {
+$("#myForm").click(function(event) {
+event.preventDefault();
+
+name = $("#name").val();
+email = $("#email").val();
+phone = $("#phone").val();
+
+if (name.length == 0 || email.length == 0 || phone.length == 0) {
+alert("Please fill out all of the required information.");
+return false;
+}
+else {
+alert("Your message has been received! You'll hear from us shortly.");
+return true;
+}
+});
+});
